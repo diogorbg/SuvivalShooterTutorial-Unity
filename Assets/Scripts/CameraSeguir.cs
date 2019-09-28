a8f4c96448234f60818e727472a88904c54791a2
Diogo RBG <diogorbg@gmail.com>
2019-09-28 10:39:06 -0300

    câmera simples

--- Assets/Scripts/CameraSeguir.cs
+++ Assets/Scripts/CameraSeguir.cs
@@ -11,8 +11,21 @@ public class CameraSeguir : MonoBehaviour {
         transform.parent = null;
     }
 
+    [ContextMenu("Seguir")]
     private void LateUpdate() {
         transform.position = alvo.position + transform.forward * z_offset;
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos() {
+        Vector3 p2 = transform.GetChild(0).position;
+        Vector3 p3 = transform.GetChild(0).GetChild(0).position;
+
+        if (alvo)
+            Gizmos.DrawLine(alvo.position, transform.position);
+        Gizmos.DrawLine(transform.position, p2);
+        Gizmos.DrawLine(p2, p3);
+    }
+#endif
 
 }
